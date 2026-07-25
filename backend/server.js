c0fe30312ba1ff7e47613db10a93c778480f7d25
PragyaPandey96 <ppragya651@gmail.com>
2026-07-25 16:50:38 +0530

    complete

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use("/api/analytics", require("./routes/analyticsRoutes"));
 
 
 // Serve frontend in production
-if(process.env.Node_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../frontend/build')));
 
     app.use((req, res) => {
